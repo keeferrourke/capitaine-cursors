61ace37fd4d399fedb278a64bd1027d534881cef
Greg White <gwhite@kupulau.com>
2016-11-22 05:29:42 -0500

    Start adding cursor templates

--- src/config/conv.py
+++ src/config/conv.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 from os import listdir
 from os.path import isfile, join
+import os
 
-files = [f for f in listdir(".") if isfile(join(".", f))]
+source_path = os.path.dirname(os.path.realpath(__file__))
+
+files = [join(source_path, f) for f in listdir(source_path) if isfile(join(source_path, f))]
 for fname in files:
     if not fname.endswith(".cursor"):
         continue
     print(fname)
     f = open(fname, "r")
-    out = open(fname+".new", "w")
+    out = open(fname.replace(".cursor", ".template"), "w")
     for line in f.readlines():
         try:
             a = line.strip().split()
@@ -18,14 +21,16 @@ for fname in files:
         except:
             continue
         new_size = 0
-        if size == 24:
-            new_size = 64
-        elif size == 48:
-             new_size = 80
+        if size == 64:
+            new_size = 100
+        elif size == 80:
+             new_size = 200
+        else:
+             print("Unknown size: ", size)
         if new_size:
             xhot = int(float(xhot)/float(size) * new_size)            
             yhot = int(float(yhot)/float(size) * new_size)
-            a[0] = str(new_size)
+            a[0] = str("<small>" if new_size == 100 else "<large>")
             a[1] = str(xhot)
             a[2] = str(yhot)
         out.write(' '.join(a)+"\n")
