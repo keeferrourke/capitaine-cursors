556525a780c183354fe8428fc05c2a96249d22ad
Efus10n <jhoffstetter@gmail.com>
2019-03-06 21:28:47 -0600

    Added white theme cursors (#29)
    Thanks @Efus10n!

--- build.sh
+++ build.sh
@@ -2,63 +2,76 @@
 
 # Capitaine cursors, macOS inspired cursors based on KDE Breeze
 # Copyright (c) 2016 Keefer Rourke <keefer.rourke@gmail.com>
+# Modified to include white cursors by Efus10n - 4 Mar 2019
+
+function create {
+	cd "$SRC"
+	mkdir -p x1 x1_25 x1_5 x2
+	cd "$SRC"/$1
+	find . -name "*.svg" -type f -exec sh -c 'inkscape -z -e "../x1/${0%.svg}.png" -w 32 -h 32 $0' {} \;
+	find . -name "*.svg" -type f -exec sh -c 'inkscape -z -e "../x1_25/${0%.svg}.png" -w 40 -w 40 $0' {} \;
+	find . -name "*.svg" -type f -exec sh -c 'inkscape -z -e "../x1_5/${0%.svg}.png" -w 48 -w 48 $0' {} \;
+	find . -name "*.svg" -type f -exec sh -c 'inkscape -z -e "../x2/${0%.svg}.png" -w 64 -w 64 $0' {} \;
+
+	cd $SRC
+
+	# generate cursors
+	if [[ "$THEME" =~ White$ ]]; then
+		BUILD="$SRC"/../dist-white
+	else BUILD="$SRC"/../dist
+	fi
+	OUTPUT="$BUILD"/cursors
+	ALIASES="$SRC"/cursorList
+
+	if [ ! -d "$BUILD" ]; then
+		mkdir "$BUILD"
+	fi
+	if [ ! -d "$OUTPUT" ]; then
+		mkdir "$OUTPUT"
+	fi
+
+	echo -ne "Generating cursor theme...\\r"
+	for CUR in config/*.cursor; do
+		BASENAME="$CUR"
+		BASENAME="${BASENAME##*/}"
+		BASENAME="${BASENAME%.*}"
+		
+		xcursorgen "$CUR" "$OUTPUT/$BASENAME"
+	done
+	echo -e "Generating cursor theme... DONE"
+
+	cd "$OUTPUT"	
+
+	#generate aliases
+	echo -ne "Generating shortcuts...\\r"
+	while read ALIAS; do
+		FROM="${ALIAS#* }"
+		TO="${ALIAS% *}"
+
+		if [ -e $TO ]; then
+			continue
+		fi
+		ln -sr "$FROM" "$TO"
+	done < "$ALIASES"
+	echo -e "Generating shortcuts... DONE"
+
+	cd "$PWD"
+
+	echo -ne "Generating Theme Index...\\r"
+	INDEX="$OUTPUT/../index.theme"
+	if [ ! -e "$OUTPUT/../$INDEX" ]; then
+		touch "$INDEX"
+		echo -e "[Icon Theme]\nName=$THEME\n" > "$INDEX"
+	fi
+	echo -e "Generating Theme Index... DONE"
+}
 
 # generate pixmaps from svg source
 SRC=$PWD/src
+THEME="Capitaine Cursors"
 
-cd "$SRC"
-mkdir -p x1 x1_25 x1_5 x2
-cd "$SRC"/svg
-find . -name "*.svg" -type f -exec sh -c 'inkscape -z -e "../x1/${0%.svg}.png" -w 32 -h 32 $0' {} \;
-find . -name "*.svg" -type f -exec sh -c 'inkscape -z -e "../x1_25/${0%.svg}.png" -w 40 -w 40 $0' {} \;
-find . -name "*.svg" -type f -exec sh -c 'inkscape -z -e "../x1_5/${0%.svg}.png" -w 48 -w 48 $0' {} \;
-find . -name "*.svg" -type f -exec sh -c 'inkscape -z -e "../x2/${0%.svg}.png" -w 64 -w 64 $0' {} \;
+create svg
 
-cd $SRC
+THEME="Capitaine Cursors - White"
 
-# generate cursors
-BUILD="$SRC"/../dist
-OUTPUT="$BUILD"/cursors
-ALIASES="$SRC"/cursorList
-
-if [ ! -d "$BUILD" ]; then
-    mkdir "$BUILD"
-fi
-if [ ! -d "$OUTPUT" ]; then
-    mkdir "$OUTPUT"
-fi
-
-echo -ne "Generating cursor theme...\\r"
-for CUR in config/*.cursor; do
-	BASENAME="$CUR"
-	BASENAME="${BASENAME##*/}"
-	BASENAME="${BASENAME%.*}"
-	
-	xcursorgen "$CUR" "$OUTPUT/$BASENAME"
-done
-echo -e "Generating cursor theme... DONE"
-
-cd "$OUTPUT"	
-
-#generate aliases
-echo -ne "Generating shortcuts...\\r"
-while read ALIAS; do
-	FROM="${ALIAS#* }"
-	TO="${ALIAS% *}"
-
-	if [ -e $TO ]; then
-		continue
-	fi
-	ln -sr "$FROM" "$TO"
-done < "$ALIASES"
-echo -e "Generating shortcuts... DONE"
-
-cd "$PWD"
-
-echo -ne "Generating Theme Index...\\r"
-INDEX="$OUTPUT/../index.theme"
-if [ ! -e "$OUTPUT/../$INDEX" ]; then
-	touch "$INDEX"
-	echo -e "[Icon Theme]\nName=Capitaine Cursors\n" > "$INDEX"
-fi
-echo -e "Generating Theme Index... DONE"
+create svg-white
